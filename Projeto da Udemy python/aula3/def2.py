78292f48474b234717b6d73a032bbcc7787c4172
jon7346 <jonathanfelipe7346@gmail.com>
2024-09-24 07:59:49 -0300

    s

---
+++ Projeto da Udemy python/aula3/def2.py
@@ -0,0 +1,6 @@
+def soma(x,y):
+    # defincção
+    print(x + y)
+soma(1,2)
+list('x')['nome', ]
+append('game') 
---
+++ Projeto da Udemy python/aula3/função def.py
@@ -0,0 +1,7 @@
+def imprimir(a,b,c):
+    print('varias1')
+    print("varias2")
+    print("varias3")
+    print("varias4")
+
+imprimir(1,2,3)

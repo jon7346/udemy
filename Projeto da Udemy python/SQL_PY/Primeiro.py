f3bfdcc94cdcf2e4f79c9e23b906917d8d0950cf
jon7346 <jonathanfelipe7346@gmail.com>
2024-12-04 11:04:38 -0300

    s

--- Projeto da Udemy python/SQL_PY/Primeiro.py
+++ Projeto da Udemy python/SQL_PY/Primeiro.py
@@ -4,9 +4,29 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent 
 DB_NAME = 'db.sqlite3'
 DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
 
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
+
+cursor.execute(
+    f'DELETE FROM   sqlite_sequence WHERE name="{TABLE_NAME}"'
+)
+#CUIDADO: fazendo delete sem where 
+cursor.execute(
+    f'DELETE FROM {TABLE_NAME}'
+)
+connection.commit()
+
+#Criar a tabela 
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT'
+    'weight REAL'
+    ')'
+)
 
 cursor.close()
 connection.close()

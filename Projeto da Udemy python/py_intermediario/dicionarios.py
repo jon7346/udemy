fe36d4c3e165313352f563e6e1d328e15363462f
jon7346 <jonathanfelipe7346@gmail.com>
2024-12-05 19:15:58 -0300

    s

--- Projeto da Udemy python/py_intermediario/dicionarios.py
+++ Projeto da Udemy python/py_intermediario/dicionarios.py
@@ -31,3 +31,14 @@ keys - iterável com as chaves
 values - iteraveis
 """
 
+# Métodos úteis dos dicionários em Python
+# len - quantas chaves
+# keys - iterável com as chaves
+# values - iterável com os valores
+# items - iterável com chaves e valores
+# setdefault - adiciona valor se a chave não existe
+# copy - retorna uma cópia rasa (shallow copy)
+# get - obtém uma chave
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro

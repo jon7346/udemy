12b8f78fd08a5009271f5a5f33cccb72597701c1
jon7346 <jonathanfelipe7346@gmail.com>
2025-02-04 14:37:35 -0300

    S

---
+++ Projeto da Udemy python/py_intermediario/lista.py
@@ -0,0 +1,14 @@
+lista1 = [
+    [1,2,35,5],
+    [1,3,4,5,6,7],
+    [23,45,6,7,8,9,],
+]
+
+for lista in lista1 :
+ print( )
+ for numero in lista:
+   ante = numero 
+   if numero == ante:
+     igual = [ numero + ante + "  "] 
+      
+print(igual)
---
+++ Projeto da Udemy python/py_intermediario/sets.py
@@ -0,0 +1,3 @@
+s1 = set('luiz')
+s1 = {'luiz',1,2,3,}
+print (s1,type(s1) )

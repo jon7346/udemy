69173822601db7f44f0b10081c2f230e614fed5b
jon7346 <jonathanfelipe7346@gmail.com>
2025-02-10 11:10:01 -0300

    s

--- Projeto da Udemy python/logica_da_programação/lista de compras .py
+++ Projeto da Udemy python/logica_da_programação/lista de compras .py
@@ -2,23 +2,22 @@
 lista = []
 frase =(f'(i)inserir, (a)apagar, (l)listar ' )
 client=''
-import os
+import os 
 
 while client != 'sair':
  print(frase)
  client = input('insira um comando: ')  
  if client == 'i' :
-     os.system('clear')
      novo_item = input('insira o novo item: ')
      lista.append(novo_item)
      continue
  if client == 'a' :
-    os.system('clear')
+    
     delete = int(input('insira o indice do numero para apagar: '))
     lista.pop(delete) 
     continue
  if client == 'l' :
-    os.system('clear')
+    
     print(lista)
     continue
  else:
---
+++ Udemy_Web/javascript/aulas .js
@@ -0,0 +1,39 @@
+/*
+//null = representa a ausencia de um valor 
+var teste = nulls
+// undefined 
+var teste2 
+*/
+//desafio 
+var a = 10
+var b = 20 
+var c = null 
+
+/*
+document.write('A: ' + a+ '<br />')
+document.write('b: ' + b+ '<br />')
+document.write('c: ' + c+ '<br />')
+
+ c = a 
+ a = b 
+ b = c 
+
+document.write('A: ' + a+ '<br />')
+document.write('b: ' + b+ '<br />')
+document.write('c: ' + c+ '<br />')
+*/
+
+//ifelse 
+//operador 
+if(2 =2 && 3 >= 1 && 'a' == 'b' ){
+    document.write('verdadeiro' )
+}
+else {
+ document.write('falso')    
+}
+// operador 'ou'  é '||'
+if ( 4 == 2 || 3 >= 1 || 'a' == 'b') {
+    document.write('verdadeiro' )
+}
+else {
+ document.write('falso')}   

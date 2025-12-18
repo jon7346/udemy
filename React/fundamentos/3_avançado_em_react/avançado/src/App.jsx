720d810c5fed442bfb514ce7fb04ea0f2d2e9fe1
jon7346 <jonathan.felipe@cliconsultoria.com.br>
2025-12-18 19:20:23 -0300

    avançado

---
+++ React/fundamentos/3_avançado_em_react/avançado/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ListRender from './components/ListRender.jsx'
+function App() {
+  
+  return (
+    <div>
+      <h1>LISTA</h1>
+      <ListRender />
+    </div>
+  )
+}
+
+export default App
